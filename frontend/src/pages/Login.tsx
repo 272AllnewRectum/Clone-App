b9c7b06cf4c1614b70f720313b75d78ca80c9c31
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2026-04-09 01:59:40 +0700

    update UI and logic for login and register pages

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { Info } from "../components/Info";
 import { InputField } from "../components/InputField";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Button";
+import { login } from "../hooks/useUser";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    if (!email.trim() || !password.trim()) return;
+    try {
+      const res = await login({ email: email, password: password });
+
+      console.log(res.data);
+      localStorage.setItem("token", res.data.token);
+      console.log("Login Success!", res.data);
+      navigate("/board");
+    } catch (error: any) {
+      console.log("Login Failed!", error.response?.data || error.message);
+      alert("Wrong Email or Password!");
+    }
+  };
+
   return (
     <div className="flex h-screen justify-center items-center">
       <Info states="login" onClick={() => navigate("/register")}>
@@ -12,14 +32,18 @@ export function Login() {
           type="email"
           label="Email"
           title="Email Adress"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         ></InputField>
         <InputField
           type="password"
           label="Password"
           title="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         ></InputField>
         <div className="pt-[3vh]">
-        <Button text="Login"></Button>
+          <Button text="Login" onClick={handleSubmit}></Button>
         </div>
       </Info>
     </div>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,26 +2,49 @@ import { Info } from "../components/Info";
 import { InputField } from "../components/InputField";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Button";
+import { useState } from "react";
+import { register } from "../hooks/useUser";
 
 export function Register() {
-  const navigate = useNavigate();
-  return (
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        if(!name.trim() || !email.trim() || !password.trim()) return;
+
+        const res = await register({name: name , email: email, password:password});
+        console.log(res.data)
+        navigate("/")
+    }
+    return (
     <div className="flex h-screen justify-center items-center">
       <Info states="register" onClick={() => navigate("/login")}>
-        <InputField type="text" label="Username" title="Username"></InputField>
-        <InputField
-          type="email"
-          label="Email"
-          title="Email Adress"
-        ></InputField>
-        <InputField
-          type="password"
-          label="Password"
-          title="Password"
-        ></InputField>
-        <div className="pt-[3vh]">
-          <Button text="Register"></Button>
-        </div>
+          <InputField
+            type="text"
+            label="Username"
+            title="Username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></InputField>
+          <InputField
+            type="email"
+            label="Email"
+            title="Email Adress"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></InputField>
+          <InputField
+            type="password"
+            label="Password"
+            title="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></InputField>
+          <div className="pt-[3vh]">
+            <Button text="Register" onClick={handleSubmit}></Button>
+          </div>
       </Info>
     </div>
   );
